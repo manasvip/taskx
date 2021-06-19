d14ecb42dc67cee034253f3913ae5ee41e63f893
manasvip <manasvi12054@gmail.com>
2021-06-19 13:24:23 +0600

    TaskX: feat- Bootsplash is added for android

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { View, Text, StyleSheet, ScrollView, Image } from 'react-native';
+
+import RNBootSplash from "react-native-bootsplash";
 
 import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
-  return (
 
+  useEffect(() => {
+    setTimeout(() => {
+      RNBootSplash.hide({fade: true});
+    }, 3000);
+  }, []);
+
+  return (
     <HomeScreen />
   )
 } 
